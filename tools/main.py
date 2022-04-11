cb4e7b5fc4aa3fa2cfb7bf4767081c8b0425e7e4
RaphMcGee23 <45374320+RaphMcGee23@users.noreply.github.com>
2022-04-11 20:59:53 +0100

    updates

--- tools/main.py
+++ tools/main.py
@@ -1,6 +1,10 @@
 import os
 
 def validate():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+    print(os.listdir(dir_path))
+    print(os.listdir(os.path.join(dir_path,os.pardir)))
     if os.path.exists("../folder1"):
         print("folder1 exists")
     else: 
